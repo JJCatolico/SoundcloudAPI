2d0dcd931c89f3db0b20b764d90af2be564c119a
JJCatolico <heavyweightj@gmail.com>
2017-08-29 17:30:36 -0400

    Complete Soundcloud API

--- Soundcloud.js
+++ Soundcloud.js
@@ -1,49 +1,46 @@
-    SC.initialize({
-       client_id: 'fd4e76fc67798bfa742089ed619084a6',
-   });
+SC.initialize({
+  client_id: "fd4e76fc67798bfa742089ed619084a6"
+});
 
+SC.get("/tracks", {
+  user_id: 25156695
+}).then(function(tracks, title) {
+  console.log(title);
+  var i = 0;
 
-   SC.get('/tracks', {
-       user_id: 25156695,
-   }).then(function (tracks, title) {
-      console.log(title)
-       var i = 0;
+  nextButton = document.getElementById("next");
+  nextButton.addEventListener("click", function() {
+    i = i + 1;
 
-       nextButton = document.getElementById('next');
-       nextButton.addEventListener('click', function () {
-           i = i + 1;
-     
-            SC.stream('tracks/' + tracks[i].id).then(function (player) {
-              player.play();
-              var songTitle = document.getElementById('song-title')
-              songTitle.innerHTML = tracks[i].title
-             });
-       });
-     
-       playButton = document.getElementById('play');
-       playButton.addEventListener('click', function () {
-           SC.stream('tracks/' + tracks[0].id).then(function (player) {
-               var songTitle = document.getElementById('song-title')
-               songTitle.innerHTML = tracks[0].title
-
-               player.play();
-               pauseButton = document.getElementById('pause');
-          pauseButton.addEventListener('click', function () {
-          player.pause()
-       })
-           });
-       });
-      
-       
-     previousButton = document.getElementById('previous');
-     previousButton.addEventListener('click', function () {
-           i = i + - 1;
-     
-      SC.stream('tracks/' + tracks[i].id).then(function (player) {
-               player.play();
-               var songTitle = document.getElementById('song-title')
-               songTitle.innerHTML = tracks[i].title
-           });
-        });
+    SC.stream("tracks/" + tracks[i].id).then(function(player) {
+      player.play();
+      var songTitle = document.getElementById("song-title");
+      songTitle.innerHTML = tracks[i].title;
+    });
   });
 
+  playButton = document.getElementById("play");
+  playButton.addEventListener("click", function() {
+    SC.stream("tracks/" + tracks[i].id).then(function(player) {
+      var songTitle = document.getElementById("song-title");
+      songTitle.innerHTML = tracks[i].title;
+      player.play();
+      pauseButton = document.getElementById("pause");
+      pauseButton.addEventListener("click", function() {
+        player.play();
+        player.pause();
+      });
+    });
+  });
+
+  previousButton = document.getElementById("previous");
+  previousButton.addEventListener("click", function() {
+    i = i + -1;
+
+    SC.stream("tracks/" + tracks[i].id).then(function(player) {
+      player.play();
+      var songTitle = document.getElementById("song-title");
+      songTitle.innerHTML = tracks[i].title;
+    });
+  });
+});
